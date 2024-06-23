dfba9428494d141fd9f146e64b75cdd1006f305f
Miguel Soriano <miguel.sor.88@gmail.com>
2024-06-24 00:23:18 +1000

    done the fibonacci sequence learning data structures in Python

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonacci = []
+    for num in range(length):
+        if len(fibonacci) > 1:
+            num_to_add = fibonacci[-2] + fibonacci[-1]
+            fibonacci.append(num_to_add)
+        else:
+            fibonacci.append(num)
+    print(fibonacci)
